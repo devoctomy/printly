ff9c67590165e359c1a5a02616bcd3fb375c92ca
Nick Pateman <nick@devoctomy.com>
2020-11-28 14:29:19 +0000

    More test coverage.

---
+++ Printly.UnitTests/Printers/UpdatePrinterCommandHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Moq;
+using Printly.Domain.Services;
+using Printly.Printers;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Printly.UnitTests.Printers
+{
+    public class UpdatePrinterCommandHandlerTests
+    {
+        [Fact]
+        public async Task GivenCommand_WhenHandled_ThenDtoMappedToDomain_AndStorageCreateCalled_AndResponseReturned()
+        {
+            // Arrange
+            var mockDataStorage = new Mock<IDataStorageService<Domain.Models.Printer>>();
+            var mockMapper = new Mock<IMapper>();
+            var sut = new UpdatePrinterCommandHandler(
+                mockDataStorage.Object,
+                mockMapper.Object);
+
+            var command = new UpdatePrinterCommand()
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Printer = new Dto.Request.Printer()
+            };
+
+            var printerDomain = new Domain.Models.Printer();
+
+            mockMapper.Setup(x => x.Map<Domain.Models.Printer>(
+                It.IsAny<Dto.Request.Printer>())).Returns(printerDomain);
+
+            mockDataStorage.Setup(x => x.Update(
+                It.IsAny<string>(),
+                It.IsAny<Domain.Models.Printer>()))
+                .ReturnsAsync(new ReplaceOneResult.Acknowledged(
+                    0,
+                    null,
+                    null));
+
+            // Act
+            var response = await sut.Handle(
+                command,
+                CancellationToken.None);
+
+            // Assert
+            mockMapper.Verify(x => x.Map<Domain.Models.Printer>(
+                It.Is<Dto.Request.Printer>(y => y == command.Printer)), Times.Once);
+
+            mockDataStorage.Verify(x => x.Update(
+                It.Is<string>(y => y == command.Id),
+                It.Is<Domain.Models.Printer>(y => y == printerDomain)), Times.Once);
+
+            Assert.True(response.IsAcknowledged);
+        }
+    }
+}
