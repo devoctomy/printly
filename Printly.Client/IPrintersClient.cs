9f22bee0655bfab36e76208a022e37d2a858dc15
Nick Pateman <nick@devoctomy.com>
2020-11-29 10:55:17 +0000

    Started adding more functionality to client.  Need more suitable response object.

---
+++ Printly.Client/IPrintersClient.cs
@@ -0,0 +1,25 @@
+﻿using Printly.Dto.Response;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Printly.Client
+{
+    public interface IPrintersClient
+    {
+        Task<PrintlyResponse<List<Printer>>> GetAllAsync(CancellationToken cancellationToken);
+        Task<PrintlyResponse<Printer>> GetAsync(
+            string id,
+            CancellationToken cancellationToken);
+        Task<PrintlyResponse<Printer>> Createsync(
+            Dto.Request.Printer printer,
+            CancellationToken cancellationToken);
+        Task<PrintlyResponse<Printer>> UpdateAsync(
+            string id,
+            Dto.Request.Printer printer,
+            CancellationToken cancellationToken);
+        Task<PrintlyResponse<Printer>> DeleteAsync(
+            string id,
+            CancellationToken cancellationToken);
+    }
+}
---
+++ Printly.Client/PrintersClient.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Printly.Client
+{
+    public class PrintersClient : BaseClient, IPrintersClient
+    {
+        private readonly IHttpAdapter<PrintersClient> _httpAdapter;
+
+        public PrintersClient(IHttpAdapter<PrintersClient> httpAdapter)
+        {
+            _httpAdapter = httpAdapter;
+        }
+
+        public async Task<PrintlyResponse<Dto.Response.Printer>> Createsync(
+            Dto.Request.Printer printer,
+            CancellationToken cancellationToken)
+        {
+            var response = await _httpAdapter.PostAsync(
+                new Uri($"/api/Printers", UriKind.Relative),
+                new StringContent(JsonConvert.SerializeObject(printer), Encoding.UTF8, "application/json"),
+                cancellationToken);
+            return await ProcessHttpResponseMessage<Dto.Response.Printer>(response);
+        }
+
+        public async Task<PrintlyResponse<List<Dto.Response.Printer>>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var response = await _httpAdapter.GetAsync(
+                new Uri("/api/Printers", UriKind.Relative),
+                cancellationToken);
+            return await ProcessHttpResponseMessage<List<Dto.Response.Printer>>(response);
+        }
+
+        public async Task<PrintlyResponse<Dto.Response.Printer>> GetAsync(
+            string id,
+            CancellationToken cancellationToken)
+        {
+            var response = await _httpAdapter.GetAsync(
+                new Uri($"/api/Printers/{id}", UriKind.Relative),
+                cancellationToken);
+            return await ProcessHttpResponseMessage<Dto.Response.Printer>(response);
+        }
+
+        public async Task<PrintlyResponse<Dto.Response.Printer>> UpdateAsync(
+            string id,
+            Dto.Request.Printer printer,
+            CancellationToken cancellationToken)
+        {
+            var response = await _httpAdapter.DeleteAsync(
+                new Uri($"/api/Printers/{id}", UriKind.Relative),
+                cancellationToken);
+            return await ProcessHttpResponseMessage<Dto.Response.Printer>(response);
+        }
+
+        public async Task<PrintlyResponse<Dto.Response.Printer>> DeleteAsync(
+            string id,
+            CancellationToken cancellationToken)
+        {
+            var response = await _httpAdapter.DeleteAsync(
+                new Uri($"/api/Printers/{id}", UriKind.Relative),
+                cancellationToken);
+            return await ProcessHttpResponseMessage<Dto.Response.Printer>(response);
+        }
+    }
+}
