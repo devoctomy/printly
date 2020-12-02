04a62970fd4a8dd52631f4e7cc7da6ad97fbca2c
Nick Pateman <nick@devoctomy.com>
2020-12-02 11:05:22 +0000

    Removed more code duplication from test.

--- Printly.UnitTests/Services/SerialPortConnectionManagerTests.cs
+++ Printly.UnitTests/Services/SerialPortConnectionManagerTests.cs
@@ -9,27 +9,33 @@ namespace Printly.UnitTests.Services
 {
     public class SerialPortConnectionManagerTests
     {
+        private readonly Mock<ISerialPortCommunicationServiceFactory>  _mockSerialPortCommunicationServiceFactory;
+        private readonly Mock<ISerialPortCommunicationService> _mockSerialPortCommunicationService;
+        private readonly SerialPortConnectionManager _sut;
+        private readonly string _portName = "Bob";
+        private readonly int _baudRate = 9600;
+        private readonly Parity _parity = Parity.None;
+        private readonly int _dataBits = 8;
+        private readonly StopBits _stopBits = StopBits.One;
+        private readonly Handshake _handshake = Handshake.None;
+        private readonly TimeSpan _readTimeout = new TimeSpan(0, 1, 0);
+        private readonly TimeSpan _writeTimeout = new TimeSpan(0, 1, 0);
+
+        public SerialPortConnectionManagerTests()
+        {
+            _mockSerialPortCommunicationServiceFactory = new Mock<ISerialPortCommunicationServiceFactory>();
+            _mockSerialPortCommunicationService = new Mock<ISerialPortCommunicationService>();
+            _sut = new SerialPortConnectionManager(_mockSerialPortCommunicationServiceFactory.Object);
+
+            _mockSerialPortCommunicationServiceFactory.Setup(x => x.Create())
+                .Returns(_mockSerialPortCommunicationService.Object);
+        }
+
         [Fact]
         public void GivenPortName_AndConnectionNotCached_AndConnectionAvailable_WhenGetOrOpen_ThenConnectionOpenedAndReturned()
         {
             // Arrange
-            var mockSerialPortCommunicationServiceFactory = new Mock<ISerialPortCommunicationServiceFactory>();
-            var mockSerialPortCommunicationService = new Mock<ISerialPortCommunicationService>();
-            var sut = new SerialPortConnectionManager(mockSerialPortCommunicationServiceFactory.Object);
-
-            var portName = "Bob";
-            var baudRate = 9600;
-            var parity = Parity.None;
-            var dataBits = 8;
-            var stopBits = StopBits.One;
-            var handshake = Handshake.None;
-            var readTimeout = new TimeSpan(0, 1, 0);
-            var writeTimeout = new TimeSpan(0, 1, 0);
-
-            mockSerialPortCommunicationServiceFactory.Setup(x => x.Create())
-                .Returns(mockSerialPortCommunicationService.Object);
-
-            mockSerialPortCommunicationService.Setup(x => x.Open(
+            _mockSerialPortCommunicationService.Setup(x => x.Open(
                 It.IsAny<string>(),
                 It.IsAny<int>(),
                 It.IsAny<Parity>(),
@@ -41,41 +47,25 @@ namespace Printly.UnitTests.Services
                 .Returns(true);
 
             // Act
-            var result = sut.GetOrOpen(
-                portName,
-                baudRate,
-                parity,
-                dataBits,
-                stopBits,
-                handshake,
-                readTimeout,
-                writeTimeout);
+            var result = _sut.GetOrOpen(
+                _portName,
+                _baudRate,
+                _parity,
+                _dataBits,
+                _stopBits,
+                _handshake,
+                _readTimeout,
+                _writeTimeout);
 
             // Assert
-            Assert.Equal(mockSerialPortCommunicationService.Object, result);
+            Assert.Equal(_mockSerialPortCommunicationService.Object, result);
         }
 
         [Fact]
         public void GivenPortName_AndConnectionNotCached_AndConnectionNotAvailable_WhenGetOrOpen_ThenExceptionThrown()
         {
             // Arrange
-            var mockSerialPortCommunicationServiceFactory = new Mock<ISerialPortCommunicationServiceFactory>();
-            var mockSerialPortCommunicationService = new Mock<ISerialPortCommunicationService>();
-            var sut = new SerialPortConnectionManager(mockSerialPortCommunicationServiceFactory.Object);
-
-            var portName = "Bob";
-            var baudRate = 9600;
-            var parity = Parity.None;
-            var dataBits = 8;
-            var stopBits = StopBits.One;
-            var handshake = Handshake.None;
-            var readTimeout = new TimeSpan(0, 1, 0);
-            var writeTimeout = new TimeSpan(0, 1, 0);
-
-            mockSerialPortCommunicationServiceFactory.Setup(x => x.Create())
-                .Returns(mockSerialPortCommunicationService.Object);
-
-            mockSerialPortCommunicationService.Setup(x => x.Open(
+            _mockSerialPortCommunicationService.Setup(x => x.Open(
                 It.IsAny<string>(),
                 It.IsAny<int>(),
                 It.IsAny<Parity>(),
@@ -89,15 +79,15 @@ namespace Printly.UnitTests.Services
             // Act & Assert
             Assert.ThrowsAny<SerialPortConnectionException>(() =>
             {
-                sut.GetOrOpen(
-                    portName,
-                    baudRate,
-                    parity,
-                    dataBits,
-                    stopBits,
-                    handshake,
-                    readTimeout,
-                    writeTimeout);
+                _sut.GetOrOpen(
+                    _portName,
+                    _baudRate,
+                    _parity,
+                    _dataBits,
+                    _stopBits,
+                    _handshake,
+                    _readTimeout,
+                    _writeTimeout);
             });
         }
 
@@ -105,23 +95,7 @@ namespace Printly.UnitTests.Services
         public void GivenPortName_AndConnectionCached_AndConnectionAvailable_WhenGetOrOpen_ThenCachedConnectionReturned()
         {
             // Arrange
-            var mockSerialPortCommunicationServiceFactory = new Mock<ISerialPortCommunicationServiceFactory>();
-            var mockSerialPortCommunicationService = new Mock<ISerialPortCommunicationService>();
-            var sut = new SerialPortConnectionManager(mockSerialPortCommunicationServiceFactory.Object);
-
-            var portName = "Bob";
-            var baudRate = 9600;
-            var parity = Parity.None;
-            var dataBits = 8;
-            var stopBits = StopBits.One;
-            var handshake = Handshake.None;
-            var readTimeout = new TimeSpan(0, 1, 0);
-            var writeTimeout = new TimeSpan(0, 1, 0);
-
-            mockSerialPortCommunicationServiceFactory.Setup(x => x.Create())
-                .Returns(mockSerialPortCommunicationService.Object);
-
-            mockSerialPortCommunicationService.Setup(x => x.Open(
+            _mockSerialPortCommunicationService.Setup(x => x.Open(
                 It.IsAny<string>(),
                 It.IsAny<int>(),
                 It.IsAny<Parity>(),
@@ -132,30 +106,30 @@ namespace Printly.UnitTests.Services
                 It.IsAny<TimeSpan>()))
                 .Returns(true);
 
-            sut.GetOrOpen(
-                portName,
-                baudRate,
-                parity,
-                dataBits,
-                stopBits,
-                handshake,
-                readTimeout,
-                writeTimeout);
+            _sut.GetOrOpen(
+                _portName,
+                _baudRate,
+                _parity,
+                _dataBits,
+                _stopBits,
+                _handshake,
+                _readTimeout,
+                _writeTimeout);
 
             // Act
-            var result = sut.GetOrOpen(
-                    portName,
-                    baudRate,
-                    parity,
-                    dataBits,
-                    stopBits,
-                    handshake,
-                    readTimeout,
-                    writeTimeout);
+            var result = _sut.GetOrOpen(
+                    _portName,
+                    _baudRate,
+                    _parity,
+                    _dataBits,
+                    _stopBits,
+                    _handshake,
+                    _readTimeout,
+                    _writeTimeout);
 
             // Assert
             Assert.NotNull(result);
-            mockSerialPortCommunicationService.Verify(x => x.Open(
+            _mockSerialPortCommunicationService.Verify(x => x.Open(
                 It.IsAny<string>(),
                 It.IsAny<int>(),
                 It.IsAny<Parity>(),
@@ -170,23 +144,7 @@ namespace Printly.UnitTests.Services
         public void GivenPortName_AndConnectionOpened_WhenClose_ThenTrueReturned()
         {
             // Arrange
-            var mockSerialPortCommunicationServiceFactory = new Mock<ISerialPortCommunicationServiceFactory>();
-            var mockSerialPortCommunicationService = new Mock<ISerialPortCommunicationService>();
-            var sut = new SerialPortConnectionManager(mockSerialPortCommunicationServiceFactory.Object);
-
-            var portName = "Bob";
-            var baudRate = 9600;
-            var parity = Parity.None;
-            var dataBits = 8;
-            var stopBits = StopBits.One;
-            var handshake = Handshake.None;
-            var readTimeout = new TimeSpan(0, 1, 0);
-            var writeTimeout = new TimeSpan(0, 1, 0);
-
-            mockSerialPortCommunicationServiceFactory.Setup(x => x.Create())
-                .Returns(mockSerialPortCommunicationService.Object);
-
-            mockSerialPortCommunicationService.Setup(x => x.Open(
+            _mockSerialPortCommunicationService.Setup(x => x.Open(
                 It.IsAny<string>(),
                 It.IsAny<int>(),
                 It.IsAny<Parity>(),
@@ -197,18 +155,18 @@ namespace Printly.UnitTests.Services
                 It.IsAny<TimeSpan>()))
                 .Returns(true);
 
-            sut.GetOrOpen(
-                portName,
-                baudRate,
-                parity,
-                dataBits,
-                stopBits,
-                handshake,
-                readTimeout,
-                writeTimeout);
+            _sut.GetOrOpen(
+                _portName,
+                _baudRate,
+                _parity,
+                _dataBits,
+                _stopBits,
+                _handshake,
+                _readTimeout,
+                _writeTimeout);
 
             // Act
-            var result = sut.Close(portName);
+            var result = _sut.Close(_portName);
 
             // Assert
             Assert.True(result);
@@ -218,16 +176,10 @@ namespace Printly.UnitTests.Services
         public void GivenPortName_AndConnectionNotOpened_WhenClose_ThenFalseReturned()
         {
             // Arrange
-            var mockSerialPortCommunicationServiceFactory = new Mock<ISerialPortCommunicationServiceFactory>();
-            var mockSerialPortCommunicationService = new Mock<ISerialPortCommunicationService>();
-            var sut = new SerialPortConnectionManager(mockSerialPortCommunicationServiceFactory.Object);
-            var portName = "Bob";
-
-            mockSerialPortCommunicationServiceFactory.Setup(x => x.Create())
-                .Returns(mockSerialPortCommunicationService.Object);
+            // Nothing to arrange
 
             // Act
-            var result = sut.Close(portName);
+            var result = _sut.Close(_portName);
 
             // Assert
             Assert.False(result);
