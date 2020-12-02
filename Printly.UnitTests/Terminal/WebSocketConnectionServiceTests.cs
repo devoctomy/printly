1947882b44627dba18dd3fd64fcbd0b5507b015f
Nick Pateman <nick@devoctomy.com>
2020-12-02 18:53:47 +0000

    More test coverage.

---
+++ Printly.UnitTests/Terminal/WebSocketConnectionServiceTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using Printly.Terminal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Printly.UnitTests.Terminal
+{
+    public class WebSocketConnectionServiceTests
+    {
+        [Fact]
+        public async Task GivenHttpContext_AndIsWebSocketRequest_WhenCreate_ThenWebSocketAccepted_AndWebSocketTerminalServiceReturned()
+        {
+            // Arrange
+            var webSocketTerminalServiceConfiguration = new WebSocketTerminalServiceConfiguration
+            {
+                ReceiveBufferSize = 1024
+            };
+            var sut = new WebSocketConnectionService(webSocketTerminalServiceConfiguration);
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockWebSocket = new Mock<WebSocket>();
+            mockWebSocket.Setup(x => x.SendAsync(
+                It.IsAny<ArraySegment<byte>>(),
+                It.IsAny<WebSocketMessageType>(),
+                It.IsAny<bool>(),
+                It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(true));
+            var mockWebSocketManager = new Mock<WebSocketManager>();
+            mockWebSocketManager
+                .Setup(x => x.AcceptWebSocketAsync())
+                .Returns(Task.FromResult(mockWebSocket.Object));
+            mockWebSocketManager.SetupGet(x => x.IsWebSocketRequest)
+                .Returns(true);
+            mockHttpContext.SetupGet(x => x.WebSockets)
+                .Returns(mockWebSocketManager.Object);
+
+            // Act
+            var result = await sut.Create(mockHttpContext.Object);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GivenHttpContext_AndNotWebSocketRequest_WhenCreate_ThenNullReturned()
+        {
+            // Arrange
+            var webSocketTerminalServiceConfiguration = new WebSocketTerminalServiceConfiguration
+            {
+                ReceiveBufferSize = 1024
+            };
+            var sut = new WebSocketConnectionService(webSocketTerminalServiceConfiguration);
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockWebSocketManager = new Mock<WebSocketManager>();
+            mockWebSocketManager.SetupGet(x => x.IsWebSocketRequest)
+                .Returns(false);
+            mockHttpContext.SetupGet(x => x.WebSockets)
+                .Returns(mockWebSocketManager.Object);
+
+            // Act
+            var result = await sut.Create(mockHttpContext.Object);
+
+            // Assert
+            Assert.Null(result);
+        }
+    }
+}
